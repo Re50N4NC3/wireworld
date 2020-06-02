3252b84a777039f958bafaa63321e92fabe0381e
Re50N4NC3 <47196639+Re50N4NC3@users.noreply.github.com>
2020-06-02 23:51:35 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pygame
-import _tempFunct
 
 pygame.init()
 pygame.display.set_caption("wireWorld")
 clock = pygame.time.Clock()
 
 windowSize = [518, 518]  # windows size
-screen = pygame.display.set_mode(windowSize)  # display
+screen = pygame.display.set_mode(windowSize)  # set display
 myFont = pygame.font.SysFont("Times New Roman", 18)  # font
 
-width = 8  # size of the grid in px
-height = 8
-margin = 1  # margin between the grid
+width = 8  # size of the cell in px
+height = width
+margin = 1  # margin between the cells
 
-hSize = 48  # amount of grids
+hSize = 48  # amount of cells
 wSize = 48
 
 # create basic colors
@@ -24,10 +23,6 @@ c_white = (255, 255, 255)
 c_black = (0, 0, 0)
 c_yellow = (255, 240, 2)
 
-grid = []  # create blank grid
-elements = 2  # amount of grid types
-
-mouseState = "nothing"  # checks what action was performed by mouse
 LEFT = 1  # lmb
 RIGHT = 3  # rmb
 leftHold = False
@@ -35,6 +30,7 @@ rightHold = False
 sparkTimer = 0  # time after which you will be able to spark the wire, after building it
 
 # two dimensional array of the grid and grid data, values of the grid define type of the particle
+grid = []  # create blank grid
 for column in range(wSize+2):
     grid.append([])
     for row in range(hSize+2):
@@ -43,13 +39,16 @@ for column in range(wSize+2):
 
 # data grid
 partAmount = 5  # amount of created particle types
-partDataAmount = 5  # amount of particle stats
-partData = [[0 for x in range(partAmount)] for y in range(partDataAmount)]   # create blank particle database
-
+partDataAmount = 2  # amount of particle stats
 partCheck = [[0 for x in range(wSize+2)] for y in range(hSize+2)]   # create blank change check array
+partData = []  # create blank particle database
+for x in range(partAmount):
+    partData.append([])
+    for y in range(partDataAmount):
+        partData[x].append([])
 
 # two dimensional array of the particle database
-# 0 void###
+# 0 void ###
 arrType = 0  # changes particle type so it's faster to input database
 partData[arrType][0] = "Void"  # name
 partData[arrType][1] = c_white  # sprite/color
@@ -69,120 +68,128 @@ arrType = 3
 partData[arrType][0] = "Tail"
 partData[arrType][1] = c_blue
 
-changeCounter = 0  # amount of changes around tile
-timer = 0  # grid loop timer, to allow multiple checks after state change
-stepTime = 1  # time between state changes
+# 4 temporary head ###
+arrType = 4
+partData[arrType][0] = "TempHead"
+partData[arrType][1] = c_green
+
+changeCounter = 0
+timer = 0
+stepTime = 1
+
+
+# read position of the cell from mouse position 0 = x, 1 = y
+def mouse_pos(axis):
+    get_pos = pygame.mouse.get_pos()
+    position = get_pos[axis]  # position of the mouse
+
+    return int(position / (width + margin))  # read x cell
+
+
+# update the gird
+def update_cells():
+    for y in range(1, wSize + 1):
+        for x in range(1, hSize + 1):
+            grid[y][x] = partCheck[y][x]
+
+
+# change type of the cells during step
+def change_types():
+    for y in range(1, wSize + 1):
+        for x in range(1, hSize + 1):
+            # tail -> wire
+            if grid[y][x] == 3:
+                partCheck[y][x] = 1
+
+            # head -> tail
+            if grid[y][x] == 2:
+                partCheck[y][x] = 3
+
+            # wire -> head
+            if grid[y][x] == 1:
+                check_neighbors(x, y)
+
+
+# check neighboring cells and their type
+def check_neighbors(x, y):
+    near = 0  # amount of neighbors
+
+    for i in (-1, 0, 1):
+        for n in (-1, 0, 1):
+            if grid[y + i][x + n] == 2:
+                near += 1
+
+    if near == 1 or near == 2:
+        partCheck[y][x] = 2
+
+
+# draw the grid
+def draw_grid():
+    for x in range(1, wSize + 1):
+        for y in range(1, hSize + 1):
+            cur_tile = grid[x][y]  # read type of the current tile
+            color = partData[cur_tile][1]
+
+            # draw tile rectangle
+            cell_position = (margin + (width + margin) * x, margin + (height + margin) * y, width, height)
+            pygame.draw.rect(screen, color, cell_position)
+
+
+def click_check(button_left, button_right, spark_timer):
+    if button_left:
+        if grid[xPos][yPos] == 0:
+            grid[xPos][yPos] = 1  # grid type change to wire
+            partCheck[xPos][yPos] = 1
+            global sparkTimer
+            sparkTimer = 3
+    if button_left and spark_timer <= 0:
+        if grid[xPos][yPos] == 1:
+            grid[xPos][yPos] = 2  # grid type change to head
+            partCheck[xPos][yPos] = 2
+
+    if button_right:
+        if grid[xPos][yPos] == 1 or grid[xPos][yPos] == 2 or grid[xPos][yPos] == 3:
+            grid[xPos][yPos] = 0  # grid type change to wire
+            partCheck[xPos][yPos] = 0
+
 
 while True:  # initial loop ________________________________________________________________________________
     screen.fill(c_black)
 
-    # define step stuff
     # mouse position
-    pos = pygame.mouse.get_pos()
-    mouse_x = pos[0]  # x position of the mouse
-    mouse_y = pos[1]  # y position of the mouse
-
-    xPos = int(mouse_x / (height + margin))  # read x cell
-    yPos = int(mouse_y / (width + margin))  # read y cell
+    xPos = mouse_pos(0)
+    yPos = mouse_pos(1)
 
     # create grid loops
     timer += 1  # timer for grid loops
-    near = 0  # amount of nearby sparks
 
     if leftHold is False:
         sparkTimer -= 1  # decrement time required between wire placement and sparking
 
     if timer > stepTime:
         timer = 0
-        for column in range(1, wSize+1):
-            for row in range(1, hSize+1):
-                grid[column][row] = partCheck[column][row]  # change current type to saved one
+        update_cells()
 
     if timer > stepTime-1:
-        for column in range(1, wSize+1):
-            for row in range(1, hSize+1):
-                curTile = grid[column][row]  # read type of the current tile
-                
-                # initial type > output type
-                
-                # tail > wire
-                if grid[column][row] == 3:
-                    # grid[column][row] = 1
-                    partCheck[column][row] = 1
+        change_types()
 
-                # head > tail
-                if grid[column][row] == 2:
-                    # grid[column][row] = 3
-                    partCheck[column][row] = 3
+    # cell drawing and visual change
+    draw_grid()
 
-                # wire > head
-                if grid[column][row] == 1:
-                    near = 0  # neighbors
-                    if grid[column - 1][row] == 2:
-                        near += 1
-                    if grid[column - 1][row - 1] == 2:
-                        near += 1
-                    if grid[column + 1][row] == 2:
-                        near += 1
-                    if grid[column + 1][row + 1] == 2:
-                        near += 1
-                    if grid[column - 1][row + 1] == 2:
-                        near += 1
-                    if grid[column + 1][row - 1] == 2:
-                        near += 1
-                    if grid[column][row + 1] == 2:
-                        near += 1
-                    if grid[column][row - 1] == 2:
-                        near += 1
-
-                    if near == 1 or near == 2:
-                        # grid[column][row] = 2
-                        partCheck[column][row] = 2
-
-                # drawing and tile change
-    for column in range(1, wSize + 1):
-        for row in range(1, hSize + 1):
-            curTile = grid[column][row]  # read type of the current tile
-            color = partData[curTile][1]
-
-            # draw tile rectangle
-            pygame.draw.rect(screen, color, (margin + (width + margin) * column, margin + (height + margin) * row, width, height))
-
-    # _tempFunct.draw_text(screen, myFont, 400, 400, str(sparkTimer), c_red)  # used for debug
-
-    # check if something happened///////////////
+    # check inputs
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:  # checks mouse left click
-            mouseState = "clickLeft"
             leftHold = True
         elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:  # checks mouse left up
-            mouseState = "upLeft"
             leftHold = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:  # checks mouse right click
-            mouseState = "clickRight"
             rightHold = True
         elif event.type == pygame.MOUSEBUTTONUP and event.button == RIGHT:  # checks mouse right up
-            mouseState = "upRight"
             rightHold = False
-        else:
-            mouseState = "nothing"
 
-        if leftHold:
-            if grid[xPos][yPos] == 0:
-                grid[xPos][yPos] = 1  # grid type change to wire
-                partCheck[xPos][yPos] = 1
-                sparkTimer = 3
-        if leftHold and sparkTimer <= 0:
-            if grid[xPos][yPos] == 1:
-                grid[xPos][yPos] = 2  # grid type change to head
-                partCheck[xPos][yPos] = 2
+        click_check(leftHold, rightHold, sparkTimer)
 
-        if rightHold:
-            if grid[xPos][yPos] == 1 or grid[xPos][yPos] == 2 or grid[xPos][yPos] == 3:
-                grid[xPos][yPos] = 0  # grid type change to wire
-                partCheck[xPos][yPos] = 0
-
-    # pygame clock
+    # pygame clock and flush
     clock.tick(60)
     pygame.display.flip()
